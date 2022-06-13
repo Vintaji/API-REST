cbbe6de2a93ee89152dbe3df5bbd35e017fd3ac2
Ricardo Maciel <ricardo.santana@linkapi.com.br>
2022-06-13 15:51:54 -0300

    Last commit

--- db/create.sql
+++ db/create.sql
@@ -7,12 +7,4 @@ create table blog.post (
     date timestamp default now()
 );
 
-create table blog.user (
-    id serial primary key,
-    nome text not null,
-    sobrenome text not null,
-    email text not null,
-    date timestamp default now()
-);
-
 insert into blog.post (title, content) values ('Teste', 'Testando');
--- server/data/userData.js
+++
@@ -1,9 +0,0 @@
-const database = require('../infra/database');
-
-exports.login = function () {
-	// return database.query('select * from blog.post');
-};
-
-exports.register = function (id) {
-	// return database.oneOrNone('select * from blog.post where id = $1', [id]);
-};
