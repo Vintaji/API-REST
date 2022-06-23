b4e5a15554645f60bf765d2e2484c00db6077c69
Ricardo Maciel <ricardo.santana@linkapi.com.br>
2022-06-23 15:43:13 -0300

    Att Docker

--- server/index.js
+++ server/index.js
@@ -4,7 +4,6 @@ const port = 3000;
 
 app.use(express.json());
 app.use('/', require('./route/postsRoute'));
-app.use('/', require('./route/userRoute'));
 app.use(function (error, req, res, next) {
 	if (error.message === 'Post already exists') {
 		return res.status(409).send(e.message);
