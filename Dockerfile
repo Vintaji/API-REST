3b4581fb0188a877f94e66e473fa4211139f41d3
Cezar Augusto Mezzalira <cezar.mezzalira@linkapi.com.br>
2022-06-28 15:25:58 -0300

    Fix: Docker Compose using env variables

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,18 @@
-FROM node:alpine
+FROM node:16-alpine as base
 
-WORKDIR /usr/app
-
-COPY package*.json ./
-
-RUN npm i
-
-COPY . .
-
+WORKDIR .
+COPY package*.json /
 EXPOSE 3000
 
-CMD npm start
+FROM base as production
+ENV NODE_ENV=production
+ENV NODE_OPTIONS=--max_old_space_size=1024
+COPY . /
+CMD ["node", "bin/www"]
+
+FROM base as dev
+ENV NODE_ENV=development
+RUN npm install -g nodemon && npm install
+ENV NODE_OPTIONS=--max_old_space_size=1024
+COPY . /
+CMD ["nodemon", "bin/www"]
--- server/infra/database.js
+++ server/infra/database.js
@@ -1,10 +1,19 @@
 const pgp = require('pg-promise')();
+
+const {
+    POSTGRES_DB,
+    POSTGRES_USER,
+    POSTGRES_PASSWORD,
+    POSTGRES_HOST,
+    POSTGRES_PORT,
+} = process.env;
+
 const db = pgp({
-    host: 'localhost',
-    user: 'postgres',
-    password: '123',
-    port: '5432',
-    database: 'postgres'
+    host: POSTGRES_HOST || 'localhost',
+    port: POSTGRES_PORT || '5432',
+    database: POSTGRES_DB || 'postgres',
+    user: POSTGRES_USER || 'postgres',
+    password: POSTGRES_PASSWORD || '123',
 });
 
 module.exports = db;
